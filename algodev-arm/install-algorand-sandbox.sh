537a1214feaf3b39c38c79d45889085e0e1255e9
Juston Sky <juston.sky@gmail.com>
2022-01-19 19:15:12 -0500

    Alter script

--- algodev-arm/install-algorand-sandbox.sh
+++ algodev-arm/install-algorand-sandbox.sh
@@ -27,6 +27,7 @@ curl -L \
     "https://github.com/docker/compose/releases/download/1.29.2/\
 docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/algorand/sandbox.git
-cd sandbox
-./sandbox $1
+cd /home/$1
+git clone https://github.com/algorand/sandbox.git 
+cd sandbox \
+./sandbox $2
